cb69d7544c57ce2797f810c1c3933e443a1e987c
AmiiirCom <amhoes1378@gmail.com>
2026-04-30 11:32:56 +0330

    fix: overwrite log file on each run

--- logging_config.py
+++ logging_config.py
@@ -1,13 +1,17 @@
 # logging_config.py
 import logging
 from config import LOG_FILE
+import os
 
 def setup_logging():
+    # ایجاد پوشه لاگ در صورت نبود
+    os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
+    
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         handlers=[
-            logging.FileHandler(LOG_FILE, encoding='utf-8'),
+            logging.FileHandler(LOG_FILE, mode='w', encoding='utf-8'),  # حالت بازنویسی
             logging.StreamHandler()
         ]
     )
